c980a5376e7e6e19432622eed25b521241a461d4
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-10-10 11:55:09 +0200

    Add BenK keyvault secret lookup

--- dags/benk/environment.py
+++ dags/benk/environment.py
@@ -1,8 +1,31 @@
-from typing import List
+from typing import List, Any
 
 from airflow.kubernetes.secret import Secret
 from airflow.models import Variable
+from azure.identity import ManagedIdentityCredential
+from azure.keyvault.secrets import SecretClient
 from kubernetes.client import V1EnvVar
+
+
+class EnvVariable(Variable):
+
+    keyvault = SecretClient(
+        vault_url=Variable.get("BENK_KEYVAULT_URL"),
+        credential=ManagedIdentityCredential(client_id=Variable.get("BENK_AIRFLOW_CLIENTID"))
+    )
+
+    @classmethod
+    def get(
+        cls,
+        key: str,
+        default_var: Any = Variable.__NO_DEFAULT_SENTINEL,
+        deserialize_json: bool = False,
+        version: str = None
+    ) -> Any:
+        if secret := getattr(cls.keyvault.get_secret(key, version), "value", ""):
+            return secret
+
+        super().get(key, default_var, deserialize_json)
 
 
 class OperatorEnvironment:
@@ -33,7 +56,7 @@ class OperatorEnvironment:
 class GenericEnvironment(OperatorEnvironment):
     """Miscellaneous settings shared between containers"""
 
-    GOB_SHARED_DIR = Variable.get("GOB-SHARED-DIR", "/app/shared")
+    GOB_SHARED_DIR = EnvVariable("GOB-SHARED-DIR", "/app/shared")
 
 
 class GOBEnvironment(OperatorEnvironment):
@@ -43,21 +66,20 @@ class GOBEnvironment(OperatorEnvironment):
     gobupload and not in gobconfig. Gobconfig uses GOB_DATABASE_* as env var
     names, instead of DATABASE_*.
     """
-
-    DATABASE_USER = Variable.get("DATABASE-USER", "gob")
-    DATABASE_NAME = Variable.get("DATABASE-NAME", "gob")
-    DATABASE_PASSWORD = Variable.get("DATABASE-PASSWORD")
-    DATABASE_HOST_OVERRIDE = Variable.get(
+    DATABASE_USER = EnvVariable("DATABASE-USER", "gob")
+    DATABASE_NAME = EnvVariable("DATABASE-NAME", "gob")
+    DATABASE_PASSWORD = EnvVariable("DATABASE-PASSWORD")
+    DATABASE_HOST_OVERRIDE = EnvVariable(
         "DATABASE-HOST-OVERRIDE", "host.docker.internal"
     )
-    DATABASE_PORT_OVERRIDE = Variable.get("DATABASE-PORT-OVERRIDE", "5406")
+    DATABASE_PORT_OVERRIDE = EnvVariable("DATABASE-PORT-OVERRIDE", "5406")
 
 
 class GrondslagEnvironment(OperatorEnvironment):
     """Settings and secrets to connect to the grondslag database."""
 
-    GRONDSLAG_DATABASE_HOST = Variable.get("GRONDSLAG-DATABASE-HOST")
-    GRONDSLAG_DATABASE_PASSWORD = Variable.get("GRONDSLAG-DATABASE-PASSWORD")
-    GRONDSLAG_DATABASE = Variable.get("GRONDSLAG-DATABASE")
-    GRONDSLAG_DATABASE_PORT = Variable.get("GRONDSLAG-DATABASE-PORT", "1521")
-    GRONDSLAG_DATABASE_USER = Variable.get("GRONDSLAG-DATABASE-USER")
+    GRONDSLAG_DATABASE_HOST = EnvVariable("GRONDSLAG-DATABASE-HOST")
+    GRONDSLAG_DATABASE_PASSWORD = EnvVariable("GRONDSLAG-DATABASE-PASSWORD")
+    GRONDSLAG_DATABASE = EnvVariable("GRONDSLAG-DATABASE")
+    GRONDSLAG_DATABASE_PORT = EnvVariable("GRONDSLAG-DATABASE-PORT", "1521")
+    GRONDSLAG_DATABASE_USER = EnvVariable("GRONDSLAG-DATABASE-USER")
