89e31213bdd5eebb3a7514c6fe12e999e03d345e
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-12-12 16:16:26 +0100

    Add pydantic module
    Otherwise not available during runtime

--- test.sh
+++ test.sh
@@ -7,10 +7,16 @@ echo "Running mypy"
 mypy "${DAGS_PATH}"
 
 echo "Running tests"
-pytest -s tests/
+export COVERAGE_FILE=/tmp/.coverage
+
+echo "Running unit tests"
+coverage run --source=$DAGS_PATH -m pytest -s tests/
+
+echo "Coverage report"
+coverage report --show-missing --fail-under=100
 
 echo "Check if black finds no potential reformat fixes"
 black --check ${DAGS_PATH}
 
 echo "Running flake8"
-flake8 dags/
+flake8 $DAGS_PATH
