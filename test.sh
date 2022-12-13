3b9e11dc4d724623df3b42890a812aa67fceda67
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-12-13 14:17:08 +0100

    Update repo check settings
    - black
    - flake8
    - mypy
    - isort
    - pytest / coverage

--- test.sh
+++ test.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -e
-DAGS_PATH="dags/benk/"
-export PYTHONPATH=${PYTHONPATH}:dags/
 
 echo "Running mypy"
-mypy "${DAGS_PATH}"
+mypy
 
-echo "Running tests"
-pytest -s tests/
+echo "Running unit tests"
+coverage run -m pytest
+
+echo "Coverage report"
+coverage report
 
 echo "Check if black finds no potential reformat fixes"
-black --check ${DAGS_PATH}
+black --check dags/benk
 
 echo "Running flake8"
-flake8 dags/
+flake8 dags/benk
