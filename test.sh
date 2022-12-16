b341418277f8a396c404a958225ee3e9723576bb
Roel de Vries <roel.de.vries@amsterdam.nl>
2022-12-16 10:47:23 +0100

    Add isort to test.sh

--- test.sh
+++ test.sh
@@ -10,6 +10,9 @@ python -m pytest
 echo "Check if black finds no potential reformat fixes"
 black --check dags/benk
 
+echo "Check for potential import sort"
+isort --check --diff dags/benk
+
 echo "Running flake8"
 flake8 dags/benk
 
