9c7f98617fe009d7ae0db20db81ed14eb2521415
Roel de Vries <77724088+roel-devries@users.noreply.github.com>
2024-01-08 18:17:32 +0100

    Increase retry_delay

--- dags/benk/common.py
+++ dags/benk/common.py
@@ -9,7 +9,7 @@ BaseOperaterArgs: Final = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
-    "retry_delay": timedelta(seconds=30),
+    "retry_delay": timedelta(seconds=300),
 }
 
 TEAM_NAME: Final = "BenK"
