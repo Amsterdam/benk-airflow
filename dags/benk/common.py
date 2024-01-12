2c0f2f5b831fd7f7a97bea0be1eff9ecf52e31f7
Roel de Vries <77724088+roel-devries@users.noreply.github.com>
2024-01-12 14:08:56 +0100

    Set retries to 0

--- dags/benk/common.py
+++ dags/benk/common.py
@@ -8,7 +8,7 @@ BaseOperaterArgs: Final = {
     "email": ["ois.gob@amsterdam.nl"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 0,
     "retry_delay": timedelta(seconds=300),
 }
 
