299449a8c990ebb8e66fb8ee7a35286255912239
Roel de Vries <roel.de.vries@amsterdam.nl>
2024-01-26 13:01:50 +0100

    Change type string to enum for relate_mode

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -66,7 +66,7 @@ class TestGenerate:
                 schedule="my schedule"
             )
 
-            mock_param.assert_called_with(type="string", default="update", description="update or full")
+            mock_param.assert_called_with(enum=["update", "full"], default="update")
 
             mock_cross_downstream.assert_has_calls([
                 call(["initialise-"], ["import-nap_peilmerken_Grondslag"]),
